f42d69b481a92c796e6736a69892597d77a097e5
Laxsan93 <Laxsan.sandrauthaiam@etf.fr>
2026-02-03 14:45:56 +0100

    Correction impression 5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etf-v3.6';
+const CACHE_NAME = 'etf-v3.7';
 const ASSETS = ['index.html', 'accueil.css', 'accueil.js', 'formulaire.html', 'formulaire.css', 'formulaire.js', 'ETFlogo.png', 'manifest.json'];
 self.addEventListener('install', (e) => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); return self.clients.claim(); });
