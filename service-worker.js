cf77cec8f799d9923b119b95a1deb9b09987b8e0
Laxsan93 <Laxsan.sandrauthaiam@etf.fr>
2026-02-09 15:06:31 +0100

    MAJ logo

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etf-v4.7'; // CHANGEMENT ICI
+const CACHE_NAME = 'etf-v4.8'; // CHANGEMENT ICI
 const ASSETS = ['index.html','accueil.css','accueil.js','formulaire.html','formulaire.css','formulaire.js','ETFlogo.png','manifest.json'];
 self.addEventListener('install', (e) => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); return self.clients.claim(); });
